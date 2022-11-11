b64648b54f46d03f2d91ec2880c9c3dbc645580e
Hyungchul You <hyou3@gmail.com>
2022-11-11 14:54:18 -0500

    create another discount policy -> RateDiscountPolicy There are a little slight hiccups here. DIP, OCP...

--- src/main/java/Basic_Spring/demo/order/OrderServiceImpl.java
+++ src/main/java/Basic_Spring/demo/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package Basic_Spring.demo.order;
 
 import Basic_Spring.demo.discount.DiscountPolicy;
 import Basic_Spring.demo.discount.FixDiscountPolicy;
+import Basic_Spring.demo.discount.RateDiscountPolicy;
 import Basic_Spring.demo.member.Member;
 import Basic_Spring.demo.member.MemberRepository;
 import Basic_Spring.demo.member.MemoryMemberRepository;
@@ -9,7 +10,19 @@ import Basic_Spring.demo.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    /*
+    * 현재 OrderServiceImpl 은 DiscountPolicy 인터페이스 뿐만 아니라
+    * FixDiscountPolicy 구현 클래스도 의존하고 있다.
+    * DIP 위반!
+    * Dependency Inversion Principle ( 의존성 역전의 원칙 )
+    *
+    * 정책을 변경하려고 RateDiscountPolicy 로 변경하는 순간
+    * OrderServiceImpl 소스 코드가 변경되야 한다.
+    * OCP 위반!
+    * Open Close Principle ( 개방폐쇄의 원칙 )
+    * */
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
---
+++ src/test/java/Basic_Spring/demo/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,40 @@
+package Basic_Spring.demo.discount;
+
+import Basic_Spring.demo.member.Grade;
+import Basic_Spring.demo.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("10% discount should be applied to VIP")
+    public void vip_0(){
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("discount must not be applied to people not VIP")
+    public void vip_x(){
+        //given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
+}
