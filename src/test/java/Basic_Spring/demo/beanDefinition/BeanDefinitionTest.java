d36ef8bbe6097811d6846a6d6239fbe925766828
Hyungchul You <hyou3@gmail.com>
2022-11-14 16:18:39 -0500

    check bean meta info

---
+++ src/test/java/Basic_Spring/demo/beanDefinition/BeanDefinitionTest.java
@@ -0,0 +1,26 @@
+package Basic_Spring.demo.beanDefinition;
+
+import Basic_Spring.demo.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanDefinitionTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("check bean meta info")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName +
+                        " beanDefinition = " + beanDefinition );
+            }
+        }
+    }
+}
