6d7680466d7a1743945bf0dc13f43b34ad8a64a0
Hyungchul You <hyou3@gmail.com>
2022-11-14 16:02:02 -0500

    try to build xml config file

---
+++ src/test/java/Basic_Spring/demo/xml/XmlAppContext.java
@@ -0,0 +1,17 @@
+package Basic_Spring.demo.xml;
+
+import Basic_Spring.demo.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    public void xmlAppContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
